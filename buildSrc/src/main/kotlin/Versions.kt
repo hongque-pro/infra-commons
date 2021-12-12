afaa641ef079171e23b1a1f23568752fae1e122f
endink <sharping@outlook.com>
2021-12-12 23:49:25 +0800

    update plugin to fix dependency version declare

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,8 +6,8 @@
  */
 object Versions {
     var kotlin = "1.6.0"
-    var infraBom = "2.6.1"
-    var infraOrm = "1.0.0"
+    var infraBom = "2.6.3"
+    var infraOrm = "1.0.1"
     var ksp = "1.6.0-1.0.1"
-    var infraPlugin = "1.0.4"
+    var infraPlugin = "1.0.7"
 }
